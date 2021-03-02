7b1ba0e3448970799d4cbe41549fccae2a465f91
apanesar11 <as2panes@uwaterloo.ca>
2021-03-02 00:14:44 -0500

    changed java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.example.kotlin"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_6
+java.sourceCompatibility = JavaVersion.VERSION_12
 
 repositories {
 	mavenCentral()
